336a9d71d531aca4047efe7e8b7746e481164c77
lisha3@codemao.cn <lisha3@codemao.cn>
2021-03-16 19:17:36 +0800

    todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,105 @@
-import React,{createContext,useState,useMemo,useEffect,useRef} from 'react'
+import React, {
+  createContext,
+  useState,
+  useCallback,
+  useMemo,
+  useEffect,
+  useRef
+} from 'react'
 import Son from './components/son.tsx'
-export const ThemeContext=createContext('lithg')
-function App() {
-  const [count,setCount]=useState(0)
-  const it=useRef()
-  const double =useMemo(()=>{
-    return count*2
-  },[count===3])
-  useEffect(() => {
-    it.current=setInterval(()=>{
-      setCount(count=>count+1)
-    },1000)
-  }, [])
-    useEffect(() => {
-    if(count>=5){
-      clearInterval(it.current)
-    }
-  }, )
+export const ThemeContext = createContext('lithg')
+let id = new Date()
+function Control(props) {
+  const { addTodo } = props
+  const inputRef = useRef()
+  const onSubmit = (e) => {
+    e.preventDefault()
+    const newText = inputRef.current.value
+    addTodo({
+      id: ++id,
+      complete: false,
+      text: newText
+    })
+    inputRef.current.value=''
+  }
   return (
-    <div className="App">
-      <button onClick={()=>setCount(count+1)}>click+1</button><br/>
-      Count:{count}----double:{double}
-     <ThemeContext.Provider value={'lithggggg'}>
-      <Son />
-     </ThemeContext.Provider>
+    <div>
+      <h1>todos</h1>
+      <form action="" onSubmit={(e) => onSubmit(e)}>
+        <input type="text" placeholder="add todos" ref={inputRef} />
+        <button></button>
+      </form>
     </div>
-  );
+  )
+}
+function TodoItem(props){
+ const {todo:{
+   id,text,complete
+ },
+  toggleTodo,
+  removeTode}=props;
+  return (
+    <li>
+      <input type="checkbox" onChange={()=>toggleTodo(id)} checked={complete}/>
+      <label htmlFor="" style={complete?{texDecoration:'line-through' }:{}}>{text}</label>
+      <button onClick={()=>removeTode(id)}> X</button>
+    </li>
+  )
+}
+function Todos(props) {
+  const { todos, removeTode, toggleTodo } = props
+  return (
+    <div>
+      <ul>
+        {todos.map((todo) => {
+          return <TodoItem key={todo.id} todo={todo}  toggleTodo={toggleTodo}
+          removeTode={removeTode}></TodoItem>
+        })}
+      </ul>
+    </div>
+  )
+}
+function TodoLists() {
+  const [todos, setTodos] = useState([])
+  const addTodo = (todo) => {
+    setTodos((todos) => [...todos, todo])
+  }
+  const removeTode = useCallback((id) => {
+    setTodos((todos) =>
+      todos.filter((todo) => {
+        return todo.id !== id
+      })
+    )
+  }, [])
+  const toggleTodo = (id) => {
+    setTodos((todos) =>
+      todos.map((todo) => {
+        return todo.id === id ? { ...todo, complete: !todo.complete } : todo
+      })
+    )
+  }
+  useEffect(()=>{
+    setTodos( JSON.parse(localStorage.getItem('todos')||[]))
+   },[])
+  useEffect(()=>{
+    localStorage.setItem('todos',JSON.stringify(todos))
+  },[todos])
+
+  
+
+  return (
+    <div>
+      <Control addTodo={addTodo}></Control>
+      <Todos
+        toggleTodo={toggleTodo}
+        removeTode={removeTode}
+        todos={todos}
+      ></Todos>
+    </div>
+  )
+}
+function App() {
+  return <TodoLists></TodoLists>
 }
 
-export default App;
+export default App
